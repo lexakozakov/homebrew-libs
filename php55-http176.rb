1f2795ba44df250c7f8de25f2dd007296dbd9284
Lexa Kozakov <lexakozakov@gmail.com>
2014-09-29 17:47:46 +0300

    test

--- php55-http176.rb
+++ php55-http176.rb
@@ -7,41 +7,17 @@ class Php55Http176 < AbstractPhp55Extension
   sha1 'e6c8e75d770019ed5e774829750e609c7df7f6c4'
   head 'https://git.php.net/repository/pecl/http/pecl_http.git'
 
-  depends_on 'curl' => :build
-  depends_on 'libevent' => :build
-  depends_on 'php55-raphf'
-  depends_on 'php55-propro'
-
-  # overwrite the config file name to ensure extension is loaded after dependencies
-  def config_filename; "zzz_ext-" + extension + ".ini"; end
 
   def install
-    Dir.chdir "pecl_http-#{version}" unless build.head?
+      Dir.chdir "pecl_http-#{version}" unless build.head?
 
-    ENV.universal_binary if build.universal?
+      ENV.universal_binary if build.universal?
 
-    safe_phpize
-
-    # link in the raphf extension header
-    system "mkdir -p ext/raphf"
-    cp "#{Formula['php55-raphf'].opt_prefix}/include/php_raphf.h", "ext/raphf/php_raphf.h"
-
-    # link in the propro extension header
-    system "mkdir -p ext/propro"
-    cp "#{Formula['php55-propro'].opt_prefix}/include/php_propro.h", "ext/propro/php_propro.h"
-
-    system "./configure", "--prefix=#{prefix}",
-                          phpconfig,
-                          "--with-libevent-dir=#{Formula['libevent'].opt_prefix}",
-                          "--with-curl-dir=#{Formula['curl'].opt_prefix}"
-    system "make"
-    prefix.install "modules/http.so"
-    write_config_file if build.with? "config-file"
-
-    # remove old configuration file
-    old_config_filepath = config_scandir_path / "ext-http.ini"
-    if File.exist?(old_config_filepath)
-      system "unlink " + old_config_filepath
+      safe_phpize
+      system "./configure", "--prefix=#{prefix}",
+                            phpconfig
+      system "make"
+      prefix.install "modules/http.so"
+      write_config_file unless build.include? "without-config-file"
     end
-  end
 end
